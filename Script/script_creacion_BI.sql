5d11c20df414f170a090e84ba330f4d29fd4300f
guidosegesso <guido.segesso@hotmail.com>
2023-06-25 16:15:02 -0300

    Vista PromedioMensualAsegurado, MontoTotalCupones

--- Script/script_creacion_BI.sql
+++ Script/script_creacion_BI.sql
@@ -13,7 +13,10 @@ IF OBJECT_ID('FUSECHUDA.MontoCuponesReclamos','V') IS NOT NULL
   DROP VIEW FUSECHUDA.MontoCuponesReclamos
 IF OBJECT_ID('FUSECHUDA.ReclamosMensuales','V') IS NOT NULL
   DROP VIEW FUSECHUDA.ReclamosMensuales
-  
+IF OBJECT_ID('FUSECHUDA.MontoTotalCupones','V') IS NOT NULL
+  DROP VIEW FUSECHUDA.MontoTotalCupones
+IF OBJECT_ID('FUSECHUDA.PromedioMensualAsegurado','V') IS NOT NULL
+  DROP VIEW FUSECHUDA.PromedioMensualAsegurado  
 
 -- Borrado de tablas
 IF OBJECT_ID('FUSECHUDA.BI_Tiempo','U') IS NOT NULL
@@ -54,7 +57,8 @@ IF OBJECT_ID('FUSECHUDA.BI_Reclamos','U') IS NOT NULL
   DROP TABLE FUSECHUDA.BI_Reclamos   
 IF OBJECT_ID('FUSECHUDA.BI_CuponesReclamos','U') IS NOT NULL
   DROP TABLE FUSECHUDA.BI_CuponesReclamos  
-  
+IF OBJECT_ID('FUSECHUDA.BI_CuponesPedidos','U') IS NOT NULL
+  DROP TABLE FUSECHUDA.BI_CuponesPedidos    
   
 GO
 -------------------------------------------------------------------------------------------------------------------------
@@ -89,7 +93,8 @@ CREATE TABLE FUSECHUDA.BI_Rango_Horario
 (
 	ID_RANGO_HORARIO [decimal](18, 0) IDENTITY(1,1) NOT NULL ,
 	HORA_INICIAL time,
-	HORA_FINAL time
+	HORA_FINAL time,
+	DESCRIPCION NVARCHAR(50)
 )
 GO
 
@@ -107,7 +112,8 @@ CREATE TABLE FUSECHUDA.BI_Rango_Etario
 (
 	ID_RANGO_ETARIO [decimal](18, 0) IDENTITY(1,1) NOT NULL,
 	EDAD_INICIAL [decimal](18, 0),
-	EDAD_FINAL [decimal](18, 0)
+	EDAD_FINAL [decimal](18, 0),
+	DESCRIPCION NVARCHAR(25)
 )
 GO
 
@@ -196,7 +202,6 @@ CREATE TABLE FUSECHUDA.BI_CuponesReclamos
 (
 	CUPON_NRO [decimal](18, 0),
 	RECLAMO_NRO [decimal](18, 0),
-	ID_USUARIO [decimal](18, 0),
 	TIPO_CUPON [decimal](18, 0),
 	MONTO [decimal](18, 2),
 	FECHA_ALTA DATETIME,
@@ -204,6 +209,16 @@ CREATE TABLE FUSECHUDA.BI_CuponesReclamos
 )
 GO
 
+CREATE TABLE FUSECHUDA.BI_CuponesPedidos
+(
+	CUPON_NRO [decimal](18, 0),
+	PEDIDO_NRO [decimal](18, 0),
+	TIPO_CUPON NVARCHAR(50),
+	MONTO [decimal](18, 2),
+	FECHA_ALTA DATETIME,
+	FECHA_VENCIMIENTO DATETIME
+)
+GO
 
 -------------------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------Sección 3--------------------------------------------------------
@@ -224,19 +239,19 @@ INSERT INTO FUSECHUDA.BI_Dia (ID_DIA, DIA)
 SELECT ID_DIA, DIA FROM FUSECHUDA.DIAS
 GO
 
-INSERT INTO FUSECHUDA.BI_Rango_Horario (HORA_INICIAL, HORA_FINAL)
-SELECT '00:00:00', '01:59:59' UNION ALL
-SELECT '02:00:00', '03:59:59' UNION ALL
-SELECT '04:00:00', '05:59:59' UNION ALL
-SELECT '06:00:00', '07:59:59' UNION ALL
-SELECT '08:00:00', '09:59:59' UNION ALL
-SELECT '10:00:00', '11:59:59' UNION ALL
-SELECT '12:00:00', '13:59:59' UNION ALL
-SELECT '14:00:00', '15:59:59' UNION ALL
-SELECT '16:00:00', '17:59:59' UNION ALL
-SELECT '18:00:00', '19:59:59' UNION ALL
-SELECT '20:00:00', '21:59:59' UNION ALL
-SELECT '22:00:00', '23:59:59' 
+INSERT INTO FUSECHUDA.BI_Rango_Horario (HORA_INICIAL, HORA_FINAL, DESCRIPCION)
+SELECT '00:00:00', '01:59:59', '00:00 - 01:59'  UNION ALL
+SELECT '02:00:00', '03:59:59', '02:00 - 03:59' UNION ALL
+SELECT '04:00:00', '05:59:59', '04:00 - 05:59' UNION ALL
+SELECT '06:00:00', '07:59:59', '06:00 - 07:59' UNION ALL
+SELECT '08:00:00', '09:59:59', '08:00 - 09:59' UNION ALL
+SELECT '10:00:00', '11:59:59', '10:00 - 11:59' UNION ALL
+SELECT '12:00:00', '13:59:59', '12:00 - 13:59' UNION ALL
+SELECT '14:00:00', '15:59:59', '14:00 - 15:59' UNION ALL
+SELECT '16:00:00', '17:59:59', '16:00 - 17:59' UNION ALL
+SELECT '18:00:00', '19:59:59', '18:00 - 19:59' UNION ALL
+SELECT '20:00:00', '21:59:59', '20:00 - 21:59' UNION ALL
+SELECT '22:00:00', '23:59:59', '22:00 - 23:59' 
 GO
 
 INSERT INTO FUSECHUDA.BI_Provincia_Localidad (ID_PROVINCIA, NOMBRE_PROVINCIA, ID_LOCALIDAD, NOMBRE_LOCALIDAD)
@@ -245,11 +260,11 @@ FROM FUSECHUDA.LOCALIDAD loc
 LEFT JOIN FUSECHUDA.PROVINCIA prov ON prov.ID_PROVINCIA = loc.ID_PROVINCIA
 GO
 
-INSERT INTO FUSECHUDA.BI_Rango_Etario (EDAD_INICIAL, EDAD_FINAL)
-SELECT 0, 24  UNION ALL
-SELECT 25, 34 UNION ALL
-SELECT 35, 55 UNION ALL
-SELECT 56, 200
+INSERT INTO FUSECHUDA.BI_Rango_Etario (EDAD_INICIAL, EDAD_FINAL, DESCRIPCION)
+SELECT 0, 24 , '< 24' UNION ALL
+SELECT 25, 34, '25 - 34'  UNION ALL
+SELECT 35, 55, '35 - 55' UNION ALL
+SELECT 56, 200, '> 55'
 GO
 
 INSERT INTO FUSECHUDA.BI_Tipo_Medio_de_Pago (ID_MEDIO_DE_PAGO, TIPO)
@@ -294,9 +309,16 @@ SELECT ID_USUARIO, DNI, NOMBRE, APELLIDO, TELEFONO, MAIL, FECHA_NAC, FECHA_REGIS
 FROM FUSECHUDA.USUARIO
 GO
 
-INSERT INTO FUSECHUDA.BI_CuponesReclamos (CUPON_NRO, RECLAMO_NRO, ID_USUARIO, TIPO_CUPON, MONTO, FECHA_ALTA, FECHA_VENCIMIENTO)
-SELECT NUMERO_RECLAMO_CUPON, NUMERO_RECLAMO, ID_USUARIO, ID_TIPO_CUPON_RECLAMO, MONTO_RECLAMO, FECHA_RECLAMO_ALTA, FECHA_RECLAMO_VENCIMIENTO
+INSERT INTO FUSECHUDA.BI_CuponesReclamos (CUPON_NRO, RECLAMO_NRO, TIPO_CUPON, MONTO, FECHA_ALTA, FECHA_VENCIMIENTO)
+SELECT NUMERO_RECLAMO_CUPON, NUMERO_RECLAMO, ID_TIPO_CUPON_RECLAMO, MONTO_RECLAMO, FECHA_RECLAMO_ALTA, FECHA_RECLAMO_VENCIMIENTO
 FROM FUSECHUDA.CUPON_DESC_RECLAMO
+GO
+
+INSERT INTO FUSECHUDA.BI_CuponesPedidos (CUPON_NRO, PEDIDO_NRO, TIPO_CUPON, MONTO, FECHA_ALTA, FECHA_VENCIMIENTO)
+SELECT cupped.NUMERO_CUPON, cupped.NUMERO_PEDIDO, tipo.TIPO_CUPON, cupdesc.MONTO, cupdesc.FECHA_ALTA, cupdesc.FECHA_VENCIMIENTO
+FROM FUSECHUDA.CUPON_PEDIDOS cupped
+LEFT JOIN FUSECHUDA.CUPON_DESCUENTO cupdesc ON cupdesc.NUMERO_CUPON = cupped.NUMERO_CUPON
+LEFT JOIN FUSECHUDA.TIPO_CUPON tipo ON tipo.ID_TIPO_CUPON = cupdesc.ID_TIPO
 GO
 
 -------------------------------------------------------------------------------------------------------------------------
@@ -306,8 +328,9 @@ GO
 -- Esta sección consiste en crear las tablas de hechos
 
 CREATE TABLE FUSECHUDA.BI_Pedidos(
-Tiempo [decimal](18,0),
-Dia [decimal](18,0),
+Numero [decimal](18, 0),
+Tiempo nvarchar(7),
+Dia nvarchar(20),
 RangoHorario [decimal](18,0),
 Provincia [decimal](18,0),
 Localidad [decimal](18,0),
@@ -325,8 +348,11 @@ GO
 
 INSERT INTO FUSECHUDA.BI_Pedidos 
 SELECT DISTINCT
-	(SELECT ID_TIEMPO FROM FUSECHUDA.BI_Tiempo WHERE AÑO = DATEPART(YEAR,ped.FECHA) AND MES = DATEPART(MONTH, ped.FECHA)) ID_Tiempo,
-	(SELECT ID_DIA FROM FUSECHUDA.BI_Dia WHERE UPPER(DIA) = UPPER(REPLACE(REPLACE(DATENAME(DW, ped.FECHA),'á','a'),'é','e'))) ID_Dia,	
+	ped.NUMERO_PEDIDO,
+	--(SELECT ID_TIEMPO FROM FUSECHUDA.BI_Tiempo WHERE AÑO = DATEPART(YEAR,ped.FECHA) AND MES = DATEPART(MONTH, ped.FECHA)) ID_Tiempo,
+	CONVERT(VARCHAR,DATEPART(YEAR,ped.FECHA)) + '/' + RIGHT('00' + CONVERT(VARCHAR,DATEPART(MONTH, ped.FECHA)), 2) Tiempo,
+	--(SELECT ID_DIA FROM FUSECHUDA.BI_Dia WHERE UPPER(DIA) = UPPER(REPLACE(REPLACE(DATENAME(DW, ped.FECHA),'á','a'),'é','e'))) ID_Dia,	
+	REPLACE(REPLACE(DATENAME(DW, ped.FECHA),'á','a'),'é','e') Dia,
 	(SELECT ID_RANGO_HORARIO FROM FUSECHUDA.BI_Rango_Horario WHERE CONVERT(time, ped.FECHA) BETWEEN HORA_INICIAL AND HORA_FINAL) RangoHorario,
 	LOC.ID_PROVINCIA,
 	loc.ID_LOCALIDAD,
@@ -350,8 +376,8 @@ LEFT JOIN FUSECHUDA.BI_Categoria_Tipo cat ON cat.ID_CATEGORIA = loc.ID_CATEGORIA
 GO
 
 CREATE TABLE FUSECHUDA.BI_Mensajeria(
-Tiempo [decimal](18,0),
-Dia [decimal](18,0),
+Tiempo nvarchar(7),
+Dia nvarchar(20),
 RangoHorario [decimal](18,0),
 Provincia [decimal](18,0),
 Localidad [decimal](18,0),
@@ -359,6 +385,7 @@ RangoEtarioUsuario [decimal](18,0),
 RangoEtarioRepartidor [decimal](18,0),
 TipoMediodePago [decimal](18,0),
 Paquete NVARCHAR(50),
+ValorAsegurado [decimal](18,2),
 Distancia [decimal](18,2),
 TipoMovilidad [decimal](18,0),
 EstadoMensajeria [decimal](18,0)
@@ -367,8 +394,10 @@ GO
 
 INSERT INTO FUSECHUDA.BI_Mensajeria 
 SELECT
-	(SELECT ID_TIEMPO FROM FUSECHUDA.BI_Tiempo WHERE AÑO = DATEPART(YEAR,msj.FECHA_MENSAJERIA) AND MES = DATEPART(MONTH, msj.FECHA_MENSAJERIA)) ID_Tiempo,
-	(SELECT ID_DIA FROM FUSECHUDA.BI_Dia WHERE UPPER(DIA) = UPPER(REPLACE(REPLACE(DATENAME(DW, msj.FECHA_MENSAJERIA),'á','a'),'é','e'))) ID_Dia,	
+	--(SELECT ID_TIEMPO FROM FUSECHUDA.BI_Tiempo WHERE AÑO = DATEPART(YEAR,msj.FECHA_MENSAJERIA) AND MES = DATEPART(MONTH, msj.FECHA_MENSAJERIA)) ID_Tiempo,
+	CONVERT(VARCHAR,DATEPART(YEAR,msj.FECHA_MENSAJERIA)) + '/' + RIGHT('00' + CONVERT(VARCHAR,DATEPART(MONTH, msj.FECHA_MENSAJERIA)), 2) Tiempo,
+	REPLACE(REPLACE(DATENAME(DW, msj.FECHA_MENSAJERIA),'á','a'),'é','e') Dia,
+	--(SELECT ID_DIA FROM FUSECHUDA.BI_Dia WHERE UPPER(DIA) = UPPER(REPLACE(REPLACE(DATENAME(DW, msj.FECHA_MENSAJERIA),'á','a'),'é','e'))) ID_Dia,	
 	(SELECT ID_RANGO_HORARIO FROM FUSECHUDA.BI_Rango_Horario WHERE CONVERT(time, msj.FECHA_MENSAJERIA) BETWEEN HORA_INICIAL AND HORA_FINAL) RangoHorario,
 	prov.ID_PROVINCIA,
 	msj.ID_LOCALIDAD,
@@ -376,6 +405,7 @@ SELECT
 	(SELECT ID_RANGO_ETARIO FROM FUSECHUDA.BI_Rango_Etario WHERE DATEDIFF(YEAR, rep.FECHA_NAC, GETDATE()) BETWEEN EDAD_INICIAL AND EDAD_FINAL) RangoEtarioRepartidor,
 	mdp.ID_MEDIO_DE_PAGO,
 	msj.PAQUETE_TIPO,
+	msj.VALOR_ASEGURADO,
 	msj.DISTANCIA,
 	rep.ID_MOVILIDAD,
 	msj.ID_ESTADO
@@ -391,8 +421,8 @@ GO
 
 CREATE TABLE FUSECHUDA.BI_Reclamos(
 Numero [decimal](18,0),
-Tiempo [decimal](18,0),
-Dia [decimal](18,0),
+Tiempo nvarchar(7),
+Dia nvarchar(20),
 RangoHorario [decimal](18,0),
 RangoEtarioUsuario [decimal](18,0),
 RangoEtarioOperario [decimal](18,0),
@@ -407,8 +437,9 @@ GO
 INSERT INTO FUSECHUDA.BI_Reclamos (Numero, Tiempo, Dia, RangoHorario, RangoEtarioUsuario, RangoEtarioOperario, Pedido, [Local], TipoReclamo, EstadoReclamo)
 SELECT 
 	rec.NUMERO_RECLAMO,
-	(SELECT ID_TIEMPO FROM FUSECHUDA.BI_Tiempo WHERE AÑO = DATEPART(YEAR,rec.FECHA) AND MES = DATEPART(MONTH, rec.FECHA)) ID_Tiempo,
-	(SELECT ID_DIA FROM FUSECHUDA.BI_Dia WHERE UPPER(DIA) = UPPER(REPLACE(REPLACE(DATENAME(DW, rec.FECHA),'á','a'),'é','e'))) ID_Dia,	
+	CONVERT(VARCHAR,DATEPART(YEAR,rec.FECHA)) + '/' + RIGHT('00' + CONVERT(VARCHAR,DATEPART(MONTH, rec.FECHA)), 2) Tiempo,
+	REPLACE(REPLACE(DATENAME(DW, rec.FECHA),'á','a'),'é','e') Dia,	
+	--(SELECT ID_DIA FROM FUSECHUDA.BI_Dia WHERE UPPER(DIA) = UPPER(REPLACE(REPLACE(DATENAME(DW, rec.FECHA),'á','a'),'é','e'))) ID_Dia,	
 	(SELECT ID_RANGO_HORARIO FROM FUSECHUDA.BI_Rango_Horario WHERE CONVERT(time, rec.FECHA) BETWEEN HORA_INICIAL AND HORA_FINAL) RangoHorario,
 	(SELECT ID_RANGO_ETARIO FROM FUSECHUDA.BI_Rango_Etario WHERE DATEDIFF(YEAR, usr.FECHA_NAC, GETDATE()) BETWEEN EDAD_INICIAL AND EDAD_FINAL) RangoEtarioUsuario,
 	(SELECT ID_RANGO_ETARIO FROM FUSECHUDA.BI_Rango_Etario WHERE DATEDIFF(YEAR, op.FECHA_NAC, GETDATE()) BETWEEN EDAD_INICIAL AND EDAD_FINAL) RangoEtarioRepartidor,
@@ -435,53 +466,82 @@ GO
 CREATE VIEW FUSECHUDA.MayorCantidadPedidos
 AS
 SELECT --* from FUSECHUDA.BI_Pedidos
-	CONVERT(VARCHAR,tiempo.AÑO) + '-' + CONVERT(VARCHAR,tiempo.MES) Tiempo, 
-	dia.DIA,
-	CONVERT(VARCHAR,rh.HORA_INICIAL) + ' - ' + CONVERT(VARCHAR,rh.HORA_FINAL)	RangoHorario,
+	ped.Tiempo, 
+	ped.dia,
+	rh.DESCRIPCION RangoHorario,
 	loc.NOMBRE_LOCALIDAD Localidad, 
 	LocalCategoria,
 	COUNT(*) CANTIDAD
 	
 FROM FUSECHUDA.BI_Pedidos ped
-LEFT JOIN FUSECHUDA.BI_Dia dia on dia.ID_DIA = ped.Dia
-LEFT JOIN FUSECHUDA.BI_Tiempo tiempo ON tiempo.ID_TIEMPO = ped.Tiempo
 LEFT JOIN FUSECHUDA.BI_Rango_Horario rh ON rh.ID_RANGO_HORARIO = ped.RangoHorario
 LEFT JOIN FUSECHUDA.BI_Provincia_Localidad loc ON loc.ID_LOCALIDAD = ped.Localidad
---WHERE Localidad = 3 AND tiempo.ID_TIEMPO = 1
-GROUP BY tiempo.AÑO, tiempo.MES, dia.DIA,	rh.HORA_INICIAL,rh.HORA_FINAL,	loc.NOMBRE_LOCALIDAD, 	LocalCategoria
-ORDER BY tiempo.AÑO, tiempo.MES, dia.DIA
+GROUP BY ped.Tiempo, ped.dia, rh.DESCRIPCION, loc.NOMBRE_LOCALIDAD, LocalCategoria
+--ORDER BY ped.Tiempo, loc.NOMBRE_LOCALIDAD, LocalCategoria
 
 GO
 
--- Promedio de calificación mensual por local. listo
+-- Monto total de los cupones utilizados por mes en función del rango etario de los usuarios
+-- LISTO
+CREATE VIEW FUSECHUDA.MontoTotalCupones
+AS
+SELECT 
+	ped.Tiempo,
+	re.DESCRIPCION RangoEtario,
+	SUM(cup.MONTO) Monto
+FROM FUSECHUDA.BI_CuponesPedidos cup
+LEFT JOIN FUSECHUDA.BI_Pedidos ped ON ped.Numero = cup.PEDIDO_NRO
+LEFT JOIN FUSECHUDA.BI_Rango_Etario re ON re.ID_RANGO_ETARIO = ped.RangoEtarioUsuario
+GROUP BY ped.Tiempo, re.DESCRIPCION
+--order by 1, 2
+GO
+
+-- Promedio de calificación mensual por local
+-- LISTO
 CREATE VIEW FUSECHUDA.CalificacionPromedio
 AS
 SELECT
-	ped.[Local],
-	ped.Tiempo,	 
+	ped.Tiempo,
+	loc.NOMBRE [Local],
 	AVG(ped.Calificacion) CalificacionPromedio
 FROM FUSECHUDA.BI_Pedidos ped
+LEFT JOIN FUSECHUDA.BI_Local loc ON loc.ID_LOCAL = ped.[Local]
 WHERE ped.Calificacion IS NOT NULL
-GROUP BY ped.Tiempo, ped.[Local]
+GROUP BY ped.Tiempo, loc.NOMBRE
+GO
+
+--Promedio mensual del valor asegurado (valor declarado por el usuario) de los paquetes enviados a través del servicio de mensajería en función del tipo de paquete
+-- LISTO
+CREATE VIEW FUSECHUDA.PromedioMensualAsegurado
+AS
+SELECT
+	Tiempo,
+	Paquete, 
+	AVG(ValorAsegurado) ValorAsegurado
+FROM FUSECHUDA.BI_Mensajeria
+GROUP BY Tiempo, Paquete
+--order by 1, 2
 GO
 
 -- Cantidad de reclamos mensuales recibidos por cada local en función del día de la semana y rango horario
+-- LISTO
 CREATE VIEW FUSECHUDA.ReclamosMensuales
 AS
 SELECT
-	--rec.Tiempo,
-	CONVERT(VARCHAR,tiempo.AÑO) + '/' + RIGHT('00' + CONVERT(VARCHAR,tiempo.MES), 2) Tiempo,
-	rec.[Local],
+	rec.Tiempo,
+	loc.NOMBRE [Local],
 	rec.Dia,
-	rec.RangoHorario,
+	rh.DESCRIPCION RangoHorario,
 	COUNT(*) Cantidad
 FROM FUSECHUDA.BI_Reclamos rec
-LEFT JOIN FUSECHUDA.BI_Tiempo tiempo ON tiempo.ID_TIEMPO = rec.Tiempo
-LEFT JOIN FUSECHUDA
-GROUP BY tiempo.AÑO, tiempo.MES, rec.Dia, rec.RangoHorario, rec.[Local]
-order by 1, rec.[Local], rec.Dia, rec.RangoHorario
+LEFT JOIN FUSECHUDA.BI_Local loc ON loc.ID_LOCAL = rec.[Local]
+LEFT JOIN FUSECHUDA.BI_Rango_Horario rh ON rh.ID_RANGO_HORARIO = rec.RangoHorario
+GROUP BY rec.Tiempo, rec.Dia, rh.DESCRIPCION, loc.NOMBRE
+--order by 1, loc.NOMBRE, dia.DIA, rh.HORA_INICIAL , rh.HORA_FINAL
 GO
--- Monto mensual generado en cupones a partir de reclamos. listo
+
+-- Monto mensual generado en cupones a partir de reclamos
+-- LISTO
 CREATE VIEW FUSECHUDA.MontoCuponesReclamos
 AS
 SELECT
